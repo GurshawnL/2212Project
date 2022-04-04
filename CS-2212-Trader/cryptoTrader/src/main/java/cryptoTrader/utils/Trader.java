5843d40bf3ccb694cbde4609df12a922e12ff2e1
uzairsalim02 <uzairsalim02@gmail.com>
2022-04-04 19:58:52 -0400

    added to trader

--- CS-2212-Trader/cryptoTrader/src/main/java/cryptoTrader/utils/Trader.java
+++ CS-2212-Trader/cryptoTrader/src/main/java/cryptoTrader/utils/Trader.java
@@ -2,20 +2,90 @@ package cryptoTrader.utils;
 
 public class Trader {
 	
-	String[][] StratConditions;
-	String[][] StratActions;
+	String[][] StratConditions = 
+			{{"BTC", ">", "57000", "ETH", "<", "4350"},
+			{"LTC", ">", "158", "DOGE", "<", "0.185"},
+			{"SOL","<","125","ETH",">","160"},
+			{"ADA","<","1.48","DOGE",">","0.19"}};
+	
+	String[][] StratActions = 
+		{{"buy", "2", "ETH"},
+		{"buy", "10", "DOGE"},
+		{"sell", "5", "SOL"},
+		{"sell", "25", "ADA"}};
 	
 	String[][] trades;
 	
 	public Trader() {
+		
 		StratConditions = new String[4][6];
 		StratActions = new String[4][3];
 		
 		trades = new String[10][7];
+		
 	}
 	
-	public boolean checKStrat(String strat) {
-		return false;		
+	public boolean checKStrat(int strat, String[] coins, Double[] prices) {
+		
+		String[] checkStrat = StratConditions[strat];
+		
+		Double checkPrice1 = Double.parseDouble(checkStrat[2]);
+		Double checkPrice2 = Double.parseDouble(checkStrat[5]);
+		
+		String cond1 = checkStrat[1];
+		String cond2 = checkStrat[4];
+		
+		//search the coin array for the wanted coin
+		for(int i=0; i<coins.length; i++) {
+			if (checkStrat[0].equals(coins[i])){ //coin1 is found
+				//check price of coin1
+				Double price1 = prices[i];
+				//compare price of coin1 to condition
+				if(cond1.equals(">")) {
+					
+				
+					if(price1 > checkPrice1) { //if condition 1 is met, check price 2
+						for(int j=0; j<coins.length; j++) {
+							if (checkStrat[0].equals(coins[j])){ //coin2 is found
+								//check price of coin2
+								Double price2 = prices[j];
+								
+								if(cond2.equals(">")) { //if both conditions are true, return true
+									if(price2>checkPrice2) {
+										return true;
+									}
+								}
+								
+					}
+				}
+				
+			}
+		}		
+				
+				else if (cond1.equals("<")) {
+					if(price1 < checkPrice1) { //if condition 1 is met, check price 2
+						for(int j=0; j<coins.length; j++) {
+							if (checkStrat[0].equals(coins[j])){ //coin2 is found
+								//check price of coin2
+								Double price2 = prices[j];
+								
+								if(cond2.equals("<")) { //if both conditions are true, return true
+									if(price2<checkPrice2) {
+										return true;
+									}
+								}
+								
+					}
+				}
+				
+			}
+				}
+		
+			}
+		
+		}
+		return false;
+		
 	}
 	
 	public void performTrade() {
